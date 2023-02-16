c4e732e19036ea464bd6c4ef0493f73d5f01bf44
Slava Chekhunov <blckct097@gmail.com>
2023-02-16 01:02:24 -0500

    first commin

---
+++ src/components/GetData.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+
+const GetData = () => {
+   const [repo, setRepo] = useState([]);
+
+    useEffect(() => {
+      const username = "Equator-Studios";
+      const repoName = "scrapers";
+      
+      axios({
+        method: "GET",
+        url: `https://api.github.com/repos/${username}/${repoName}/contents/scrapers`
+      }).then(response => {
+        setRepo(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    }, []);
+
+   return [repo];
+};
+
+export default GetData;
---
+++ src/components/GridTable.js
@@ -0,0 +1,62 @@
+import { Link } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import GetData from "./GetData";
+
+const columns = [
+  { field: "name", headerName: "Name", width: 200 },
+  {
+    field: "html_url",
+    headerName: "GitHub_URL",
+    width: 700,
+    renderCell: (params) => (
+      <Link href={params.value} target="_blank">
+        {params.value}
+      </Link>
+    ),
+  },
+  {
+    field: "download_url",
+    headerName: "Download URL",
+    width: 800,
+    cellStyle: {
+      margin: "30px",
+    },
+    renderCell: (params) => (
+      <Link href={params.value} target="_blank">
+        {params.value}
+      </Link>
+    ),
+  },
+];
+
+const TableStyles = {
+  height: "1100px",
+};
+
+const GridTable = () => {
+  const [repo] = GetData();
+
+
+  const data = repo.map((item) => ({
+    name: item.name,
+    html_url: item.html_url,
+    download_url: item.download_url,
+  }));
+
+  return (
+    <div style={{ height: 300, width: "100%" }}>
+      <DataGrid
+        getRowId={(row) => row.name}
+        rows={data}
+        columns={columns}
+        sx={TableStyles}
+        checkboxSelection
+        pagination
+        pageSize={84}
+      />
+    </div>
+  );
+
+};
+
+export default GridTable;
